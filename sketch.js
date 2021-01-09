735ca1c2defc093083afa72bd31b5b20c7e7fd23
vihangwhite <75125449+vihangwhite@users.noreply.github.com>
2021-01-09 13:46:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas,gamestate=0;
+var playerCount;
+var database,form,player,game;
+
+
+function setup(){
+ canvas = createCanvas(400,400);
+ database = firebase.database();
+ game=new Game();
+ game.getState();
+ game.start();
+ 
+}
+
+function draw() {
+
+}
